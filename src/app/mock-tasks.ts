9af56134986687d8874544574ec89426d037ec3f
AlvinNjenga <109256320+AlvinNjenga@users.noreply.github.com>
2022-09-17 19:43:58 +0100

    Adding components

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import {Task } from './Task';
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: 'Doctors appointement',
+        day: 'May 5th at 2:40pm',
+        reminder: true
+    },
+    {
+        id: 2,
+        text: 'Descale the kettle',
+        day: 'May 7th at 1:30pm',
+        reminder: true
+    },
+    {
+        id: 3,
+        text: 'Booty call',
+        day: 'May 19th at 1:00am',
+        reminder: true
+    }
+]
